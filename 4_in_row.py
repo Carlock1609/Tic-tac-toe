f8d26257214f7d40604cc50879af66f11722c6d6
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-30 20:57:51 -0700

    Connect 4 DONE! :beers:

--- 4_in_row.py
+++ 4_in_row.py
@@ -1,13 +1,13 @@
-'''
-filename: 4_in_row.py
-'''
+#! python3
+
 class Player:
     def __init__(self, name, token):
         self.name = name
         self.token = token
 
 class Game:
-    number_dict = {'10': [0, 0], '20': [0, 1], '30': [0, 2], '40': [0, 3], '50': [0, 4], '60': [0, 5], '70': [0, 6],
+    number_dict = {
+                    '10': [0, 0], '20': [0, 1], '30': [0, 2], '40': [0, 3], '50': [0, 4], '60': [0, 5], '70': [0, 6],
                     '11': [1, 0], '21': [1, 1], '31': [1, 2], '41': [1, 3], '51': [1, 4], '61': [1, 5], '71': [1, 6],
                     '12': [2, 0], '22': [2, 1], '32': [2, 2], '42': [2, 3], '52': [2, 4], '62': [2, 5], '72': [2, 6],
                     '13': [3, 0], '23': [3, 1], '33': [3, 2], '43': [3, 3], '53': [3, 4], '63': [3, 5], '73': [3, 6],
@@ -23,17 +23,18 @@ class Game:
     Ls7 = ['70','71','72','73','74','75']
 
     def __init__(self):
-        self.board =  [
+        self.board = [
             ['10','20','30','40','50','60','70'],
             ['11','21','31','41','51','61','71'],
             ['12','22','32','42','52','62','72'],
             ['13','23','33','43','53','63','73'],
             ['14','24','34','44','54','64','74'],
-            ['15','25','35','45','55','65','75']
-                ]
+            ['15','25','35','45','55','65','75'],
+                    ]
    
     def __repr__(self):
       #drawing a board 
+      print(f"C1   C2   C3   C4   C5   C6   C7 ")
       for i in self.board:
          result = " | ".join(i)
          print(result)   
@@ -41,7 +42,7 @@ class Game:
     def move(self, player):
         selected_num = 0
 
-        player_pick = input(f"{player.name}, Make your move: ")
+        player_pick = input(f"{player.name}, make your move: ")
         
         if player_pick == '1':
             selected_num = Game.Ls1[-1]
@@ -80,7 +81,8 @@ class Game:
             horizonal_ls.append(c[0])
             count = horizonal_ls.count(c[0])
             if count == 4:
-                print("you win horizontally")
+                self.__repr__()
+                print(f"{player.name} You won horizontally")
                 exit()
 
         #vertical check
@@ -90,12 +92,12 @@ class Game:
             vertical_ls.append(c[1])
             count = vertical_ls.count(c[1])
             if count == 4:
-                print("you win vertically")
+                self.__repr__()
+                print(f"{player.name} You won vertically")
                 exit()
 
         #diagonal check from lower-left o upper-right
         diag_ls = []
-        count_ll_ur = 0
 
         for i in Game.number_dict.keys():
             for n in coor_list:
@@ -103,46 +105,26 @@ class Game:
                     n = int(i)
                     diag_ls.append(n)
 
-
         for i in diag_ls:
             if diag_ls.__contains__(i+9):
                 if diag_ls.__contains__(i+18):
                     if diag_ls.__contains__(i+27):
-                        print("you're win diagonal from lower-left to upper-right.")
+                        self.__repr__()
+                        print(f"{player.name} You won diagonally from lower-left to upper-right.")
                         exit()
             if diag_ls.__contains__(i+11):
                 if diag_ls.__contains__(i+22):
                     if diag_ls.__contains__(i+33):
-                        print("you're win diagonal from upper-left to lower-right.")
+                        self.__repr__()
+                        print(f"{player.name} You won diagonally from upper-left to lower-right.")
                         exit()
-            
-
-        
-
-        #         previous_ls = i
-        #         count_ll_ur += 1
-        #     else:
-        #         if previous_ls[0] + 1 == i[0] and previous_ls[1] + 1 == i[1]:
-        #             count_ll_ur += 1
-        #             previous_ls = i
-        #             # print(count)
-        #             if count_ll_ur == 4:
-        #                 print("you win diagonal! ll - ur")
-        #                 exit()
-
-
-        # #diagonal check from upper-left to lower-right
-        #         previous_ls = []
-        #         count_ul_lr = 0
-                
-                
 
 def game_begin(board, p1, p2):
     winning_undetermined = True
     coor = []
     player1_numb_ls = []
     player2_numb_ls = []
-    print(f"To place {p1.token} or {p2.token} on the grid, enter an integer from 1-7 as a designated box on the grid.")
+    print(f"To place {p1.token} or {p2.token} on the grid, enter in an integer for column 1-7 to drop a token.")
 
    #drawing a board
     board.__repr__()
@@ -154,16 +136,10 @@ def game_begin(board, p1, p2):
         board.__repr__()
 
         #play 2 move
-
-
         coor = board.move(p2)
         player2_numb_ls.append(coor)
         board.calc_winner(p2, player2_numb_ls)        
         board.__repr__()
-
-
-
-      
 
 def main():
    print("Welcome to 4-In-A-Row!\nThis is a two-player game.\n")
